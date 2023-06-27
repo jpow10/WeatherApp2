d0dfedd9a9018ccb788557eaa90ce873e11cf29d
JuliaP <juliapowney@hotmail.co.uk>
2023-06-27 13:30:04 +0100

    JS template added
    Days in columns added but not API integrated yet.

--- src/script.js
+++ src/script.js
@@ -13,19 +13,31 @@ setInterval(updateDateTime, 1000);
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `
-    <div class="row">
-  <div class="col-2">
-  <div class="weather-forecast-date>Mon</div>
-  <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="42"/>
-  <div class="weather-forecast-temperatures">
-  <span class="weather-forecast-temperature-max">28°</span>
-  <span class="weather-forecast-temperature-min>24°</span>
-  </div>
-  </div>
-  </div>
-`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
 }
 
 function displayWeatherCondition(response) {
